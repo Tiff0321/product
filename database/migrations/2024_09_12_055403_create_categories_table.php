00ff8e2bfc6c3b00d227adadcf6a2d9fb3bf6f49
Tiff0321 <2325287709@qq.com>
2024-09-12 15:15:36 +0900

    数据库建立产品分类表

---
+++ database/migrations/2024_09_12_055403_create_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id()->comment('分类ID');
+            $table->string('name')->comment('分类名称');
+            $table->unsignedBigInteger('parent_id')->nullable()->comment('父分类ID');
+            $table->timestamps();
+
+            $table->foreign('parent_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
