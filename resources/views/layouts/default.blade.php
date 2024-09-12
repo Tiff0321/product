8cd3747617b5ef296b32b9e79d90051112817575
Tiff0321 <2325287709@qq.com>
2024-09-12 11:24:32 +0900

    优化视图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title', 'Product App')</title>
+    <title>@yield('title', 'Product App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="/">Product App</a>
-        <ul class="navbar-nav justify-content-end">
-            <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
-            <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
-        </ul>
+@include('layouts._header')
+
+<div class="container">
+    <div class="offset-md-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
     </div>
-</nav>
-@yield('content')
+</div>
 </body>
 </html>
 
