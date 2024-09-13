760010da4af2a9f187a1698aa61315ef52cd8b5a
Tiff0321 <2325287709@qq.com>
2024-09-13 21:21:15 +0900

    邮件认证邮箱

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Validation\ValidationException;
 
 class UsersController extends Controller
@@ -20,7 +21,7 @@ class UsersController extends Controller
          * 不用登录也能访问 详情页展示，用户注册界面
          */
         $this->middleware('auth',[
-            'except'=>['show','create','store','index']
+            'except'=>['show','create','store','index','confirmEmail']
         ]);
         /**
          * 只让未登录用户访问注册页面
@@ -53,10 +54,28 @@ class UsersController extends Controller
                 'password'=>bcrypt($request->password)]
         );
 
-//        $data=$request->all();--获取用户输入的所有数据
-        Auth::login($user);
-        session()->flash('success','用户创建成功');
-        return redirect()->route('users.show',[$user->id]);
+        $this->sendEmailConfirmationTo($user);
+        session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收。');
+        return redirect('/');
+
+// //       $data=$request->all();--获取用户输入的所有数据
+//        Auth::login($user);
+//        session()->flash('success','用户创建成功');
+//        return redirect()->route('users.show',[$user->id]);
+    }
+
+    protected function sendEmailConfirmationTo($user)
+    {
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = '2325287709@qq.com';
+        $name = 'tiff';
+        $to = $user->email;
+        $subject = "感谢注册 Product 应用！请确认你的邮箱。";
+
+        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
     }
 
     public function show(User $user): Factory|View|Application
@@ -113,12 +132,37 @@ class UsersController extends Controller
         return view('users.index',compact('users'));
     }
 
+    /**
+     * @throws AuthorizationException
+     */
+    public function destroy(User $user): RedirectResponse
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
+    }
+
 //    public function gravatar($size = '100'): string
 //    {
 //        $hash = md5(strtolower(trim($this->attributes['email'])));
 //        return "https://cdn.v2ex.com/gravatar/$hash?s=$size";
 //
 //    }
+
+    public function confirmEmail($token): RedirectResponse
+    {
+        $user = User::where('activation_token', $token)->firstOrFail();
+
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success', '恭喜你，激活成功！');
+        return redirect()->route('users.show', [$user]);
+
+}
 
 
 }
