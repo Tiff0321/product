d4d66771947ca260e104113a448c8bc20f78a243
Tiff0321 <2325287709@qq.com>
2024-09-13 22:17:58 +0900

    忘记密码

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,12 @@ Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
+//邮箱认证
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+//忘记密码
+Route::get('password/reset',  'PasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email',  'PasswordController@sendResetLinkEmail')->name('password.email');
+
+Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset',  'PasswordController@reset')->name('password.update');
